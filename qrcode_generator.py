0036f0571df3eb6d5003c3c8dc090ff5c8c65285
gustavord <50451202+gustavord@users.noreply.github.com>
2020-10-09 00:27:53 -0300

    Function documentation
    docstrings for generate_qrcode_image & create_qrcode

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -7,10 +7,40 @@ IMG_OUTPUT_PATH = 'static/qrcodes/'
 
 
 def generate_qrcode_image(content, err_lvl):
+"""
+    Generates the scannable visual output.
+    
+Parameters
+----------
+content: str
+        Content to be accessed through QR code
+
+err_lvl: str
+        Desired error correction level
+
+Returns
+-------
+A QR code image
+    """
     return segno.make(content, error=err_lvl)
 
 
 def create_qrcode(content, error='H'):
+    """
+    Embeds content into a QR code.
+    
+    Parameters
+    ----------
+    content: str
+            Content to be embedded into QR code
+    
+    error: str
+           QR code error correction level (default: high)
+    
+    Returns
+    -------
+    Functional QR code
+    """
     filename = IMG_OUTPUT_PATH \
         + hashlib.sha256(
             f'{content}_{datetime.now()}'.encode('utf-8')).hexdigest() \
